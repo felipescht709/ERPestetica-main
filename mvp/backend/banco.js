bb0a2a3c91791a1d3fd27573ff760373128f25c7
felipescht709 <156364363+felipescht709@users.noreply.github.com>
2025-09-21 18:36:45 -0300

    Tentativa final de corrigir build

--- mvp/backend/banco.js
+++ mvp/backend/banco.js
@@ -1,12 +1,12 @@
 // db.js
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432, 
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
